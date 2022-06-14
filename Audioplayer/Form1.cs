e6465728e90ce620e2e53dc7bb9c56d5d356e7ab
JammingEnd <57032230+JammingEnd@users.noreply.github.com>
2022-06-14 11:59:32 +0100

    oml

--- Audioplayer/Form1.cs
+++ Audioplayer/Form1.cs
@@ -16,5 +16,10 @@ namespace Audioplayer
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
---
+++ Audioplayer/Music Controllers/Mp3Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Audioplayer.Music_Controllers
+{
+    public class Mp3Player
+    {
+        public void Mp3Controller()
+        {
+
+        }
+    }
+}
