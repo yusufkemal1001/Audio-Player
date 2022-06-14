f2cabe79ff8f7598aaa168093642205f88819901
Obejah <obejah3@outlook.com>
2022-06-14 15:31:35 +0200

    Revert "Database toegevoegd"
    This reverts commit 79ae3a84a057d6c3881ee8932c2ca7a2e8d92fd1.

--- Audioplayer/Form1.Designer.cs
+++ Audioplayer/Form1.Designer.cs
@@ -29,12 +29,7 @@ namespace Audioplayer
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             this.PlayBtn = new System.Windows.Forms.Button();
-            this.audioFilesDataSet = new Audioplayer.AudioFilesDataSet();
-            this.audioFilesDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            ((System.ComponentModel.ISupportInitialize)(this.audioFilesDataSet)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.audioFilesDataSetBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // PlayBtn
@@ -47,16 +42,6 @@ namespace Audioplayer
             this.PlayBtn.UseVisualStyleBackColor = true;
             this.PlayBtn.Click += new System.EventHandler(this.PlayBtn_Click);
             // 
-            // audioFilesDataSet
-            // 
-            this.audioFilesDataSet.DataSetName = "AudioFilesDataSet";
-            this.audioFilesDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // audioFilesDataSetBindingSource
-            // 
-            this.audioFilesDataSetBindingSource.DataSource = this.audioFilesDataSet;
-            this.audioFilesDataSetBindingSource.Position = 0;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -66,8 +51,6 @@ namespace Audioplayer
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.audioFilesDataSet)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.audioFilesDataSetBindingSource)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -75,8 +58,6 @@ namespace Audioplayer
         #endregion
 
         private System.Windows.Forms.Button PlayBtn;
-        private System.Windows.Forms.BindingSource audioFilesDataSetBindingSource;
-        private AudioFilesDataSet audioFilesDataSet;
     }
 }
 
